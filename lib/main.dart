1a1f15d058880d53a4480872cd0a1df9afd9f652
noahark <dcsun57@gmail.com>
2023-09-20 22:49:46 +0900

    change method of calling loadfirestore FutureBuilder to init

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+
 // import 'package:async/async.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'dart:convert';
@@ -36,18 +37,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-
   ////////////////////////////////////////////////////////////
   // variable setting
-  late FlutterGifController controller= FlutterGifController(vsync: this);
+  late FlutterGifController controller = FlutterGifController(vsync: this);
 
-  late Future<List<String>> gifNameList ;
-  late List<String> gifNameListNow ;
+  late List<String> gifNameList;
+  bool gifNameListCalled = false;
 
-  late Future<String> urlFuture ;
-  late String urlNow ;
+  // late List<String> gifNameListNow ;
 
-  int urlIndex = 0 ;
+  late Future<String> urlFuture;
+
+  late String urlNow;
+
+  int urlIndex = 0;
 
   // read firestore
   final db = FirebaseFirestore.instance;
@@ -59,7 +62,9 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   // flutter_secure_storage
   Map<String, dynamic> nameUrlDataLocal = {};
   String? nameUrlDataLocalString = '';
-  bool callUrl = false ;
+  bool callUrl = false;
+  bool haveUrl = false;
+
   //flutter_secure_storage 사용을 위한 초기화 작업
   static final storage = new FlutterSecureStorage();
 
@@ -90,48 +95,50 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   // 짤리스트 추가되면 기존거 지우고 새거 저장
 
   // firestore - 파일명 리스트 가져오기
-  Future<List<String>> _loadFirestore(db) async{
+  void _loadFirestore(db) async {
+    // List<String> gifNameList = [];
 
-    List<String> gifNameList = [];
-
-    var rlt_test =
-    await db
+    var rlt_test = await db
         .collection("gif_name_list_by_version")
         .doc('version_001')
         .get();
 
     // null check
     if (rlt_test.data() != null) {
-      var list = List.generate(rlt_test
-          .data()
-          .length, (i) => i);
+      var list = List.generate(rlt_test.data().length, (i) => i);
 
       print('여기가 되고 있음 firestore호출');
       List<String> gifNameList_tmp = [];
 
       for (var i in list) {
-
         gifNameList_tmp.add(rlt_test.data()![i.toString()].toString());
       }
+
       gifNameList = gifNameList_tmp;
+      print('gifNameListCalled $gifNameListCalled');
+      setState(() {
+        gifNameListCalled = true;
+      });
+      print('gifNameListCalled $gifNameListCalled');
+
+      // 여기서 urlIndex 부터 +5까지 가지고 있는지 확인
+      // 없으면 불러오게
+      haveUrl = nameUrlDataLocal.keys.contains(gifNameList[urlIndex]);
 
     } else {
       gifNameList = [];
     }
-    return gifNameList;
   }
 
   // read firebase storage
-  Future<String> readStorage(String gifName) async {
+  Future<String> _readStorage(String gifName) async {
     print('url 호출');
     final pathReference = storageRef.child("version_001/$gifName");
     final _url = await pathReference.getDownloadURL();
-    callUrl = true ;
+    callUrl = true;
 
     return _url;
   }
-
-  late Future myFuture;
 
   void _callNameUrlDataLocal() async {
     print('_callNameUrlDataLocal');
@@ -143,49 +150,91 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     //user의 정보가 있다면 바로 로그아웃 페이지로 넝어가게 합니다.
     if (nameUrlDataLocalString != null) {
       print(nameUrlDataLocalString);
-      // Navigator.pushReplacement(
-      //     context,
-      //     CupertinoPageRoute(
-      //         builder: (context) => LogOutPage(
-      //           id: nameUrlDataLocalString.split(" ")[1],
-      //           pass: nameUrlDataLocalString.split(" ")[3],
-      //         )));
       nameUrlDataLocal = jsonDecode(nameUrlDataLocalString!);
       print(nameUrlDataLocal);
+
+
     } else {
       print('yet null');
     }
   }
 
   void _writeNameUrlDataLocal(nameUrlDataLocal) async {
-
     print('_writeNameUrlDataLocal work');
     String _nameUrlDataLocalString = json.encode(nameUrlDataLocal);
 
-    if (callUrl){
-
+    if (callUrl) {
       print('_writeNameUrlDataLocal real work callUrl : $callUrl');
 
       await storage.write(
         key: "urlName",
         value: _nameUrlDataLocalString,
       );
-      callUrl = false ;
+      callUrl = false;
     }
   }
 
-  //   print(a['ss']);
-  //   print(a.toString());
-  //   String jsonstringmap = json.encode(a);
-  //   print(jsonstringmap);
-  //   Map<String, dynamic> valueMap = jsonDecode(jsonstringmap);
-  //   print(valueMap['ss']);
-  //   print(valueMap['aac']);
+  Widget futureBuilderTemp(){
+
+    // bool haveUrl = nameUrlDataLocal.keys.contains(gifNameList[urlIndex]);
+
+    print('gifNameListCalled $gifNameListCalled');
+    print('haveUrl $haveUrl');
+
+    if (gifNameListCalled == false){
+      return CircularProgressIndicator();
+    } else if (haveUrl){
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Image.network(
+          nameUrlDataLocal[gifNameList[urlIndex]].toString(),
+          width: 300,
+          height: 400,
+          fit: BoxFit.contain,
+        ),
+      );
+    } else {
+      return FutureBuilder(
+          future: _readStorage(gifNameList[urlIndex]),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            //해당 부분은 data를 아직 받아 오지 못했을때 실행되는 부분을 의미한다.
+            if (snapshot.hasData == false) {
+              return CircularProgressIndicator();
+            }
+            //error가 발생하게 될 경우 반환하게 되는 부분
+            else if (snapshot.hasError) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Error: ${snapshot.error}',
+                  style: TextStyle(fontSize: 15),
+                ),
+              );
+            }
+            // 데이터를 정상적으로 받아오게 되면 다음 부분을 실행하게 되는 것이다.
+            else {
+              print('여기 되고 있음??? ${snapshot.data.toString()}');
+              print('nameUrlDataLocal $nameUrlDataLocal');
+              nameUrlDataLocal[gifNameList[urlIndex]] =
+                  snapshot.data.toString();
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.network(
+                  snapshot.data.toString(),
+                  width: 300,
+                  height: 400,
+                  fit: BoxFit.contain,
+                ),
+              );
+            }
+          });
+    }
+  }
 
   @override
   void initState() {
     controller = FlutterGifController(vsync: this);
-    WidgetsBinding.instance?.addPostFrameCallback((_) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
       controller.repeat(
         min: 0,
         max: 10,
@@ -193,12 +242,10 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       );
     });
 
-    myFuture = _loadFirestore(db);
-
-
     // call url from local
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _callNameUrlDataLocal();
+      _loadFirestore(db);
     });
 
     super.initState();
@@ -206,7 +253,6 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-
     // write url to local db when after load url
     _writeNameUrlDataLocal(nameUrlDataLocal);
 
@@ -219,93 +265,24 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           // Text('testst'),
 
           // file name list get from firestore
-          FutureBuilder(
-              future: myFuture,
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                //해당 부분은 data를 아직 받아 오지 못했을때 실행되는 부분을 의미한다.
-                if (snapshot.hasData == false) {
-                  return CircularProgressIndicator();
-                }
-                //error가 발생하게 될 경우 반환하게 되는 부분
-                else if (snapshot.hasError) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Error: ${snapshot.error}',
-                      style: TextStyle(fontSize: 15),
-                    ),
-                  );
-                }
-                // 데이터를 정상적으로 받아오게 되면 다음 부분을 실행하게 되는 것이다.
-                else {
-                  // print("이게 짤 리스트 ${snapshot.data.toString()}");
-                  gifNameListNow = snapshot.data;
-                  // 짤 파일명 리스트 랜덤 순서 적용
-                  // gifNameListNow.shuffle();
-
-                  // 있는지 확인
-                  bool haveUrl = nameUrlDataLocal.keys.contains(gifNameListNow[urlIndex]);
-
-                  return haveUrl? Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.network(
-                      nameUrlDataLocal[gifNameListNow[urlIndex]].toString(),
-                      width: 300,
-                      height: 400,
-                      fit: BoxFit.contain,
-                    ),
-                  ):FutureBuilder(
-                      future: readStorage(gifNameListNow[urlIndex]),
-                      builder: (BuildContext context, AsyncSnapshot snapshot) {
-                        //해당 부분은 data를 아직 받아 오지 못했을때 실행되는 부분을 의미한다.
-                        if (snapshot.hasData == false) {
-                          return CircularProgressIndicator();
-                        }
-                        //error가 발생하게 될 경우 반환하게 되는 부분
-                        else if (snapshot.hasError) {
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              'Error: ${snapshot.error}',
-                              style: TextStyle(fontSize: 15),
-                            ),
-                          );
-                        }
-                        // 데이터를 정상적으로 받아오게 되면 다음 부분을 실행하게 되는 것이다.
-                        else {
-                          print('여기 되고 있음??? ${snapshot.data.toString()}');
-                          print('nameUrlDataLocal $nameUrlDataLocal');
-                          nameUrlDataLocal[gifNameListNow[urlIndex]] =
-                              snapshot.data.toString();
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Image.network(
-                              snapshot.data.toString(),
-                              width: 300,
-                              height: 400,
-                              fit: BoxFit.contain,
-                            ),
-                          );
-                        }
-                      }
-                  );
-                }
-              }
-          ),
+          futureBuilderTemp(),
           ElevatedButton(
-            onPressed: (){
+            onPressed: () {
               setState(() {
                 urlIndex += 1;
               });
-              print(gifNameListNow.length);
+              print(urlIndex);
             },
-              child: Container(child: Text('다음'),),
+            child: Container(
+              child: Text('다음'),
+            ),
           ),
           ElevatedButton(
-            onPressed: (){
+            onPressed: () {
               setState(() {
                 urlIndex -= 1;
               });
+              print(urlIndex);
             },
             child: Container(
               child: Text('이전'),
@@ -317,4 +294,3 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     );
   }
 }
-
